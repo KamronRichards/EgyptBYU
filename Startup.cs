2040bf40092746f70b00d5b71a51c034d71813ea
KamronRichards <kamronrichards2@gmail.com>
2023-04-10 10:14:43 -0600

    CSP header

--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,7 @@ namespace EgyptBYU
             app.UseAuthorization();
 
             app.Use(async (context, next) => {
-                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'");
+                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self' https://cdn.jsdelivr.net; font-src 'self'; img-src 'self'; frame-src 'self'");
 
                 await next();
 
