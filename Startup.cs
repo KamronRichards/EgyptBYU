223136e4fccf035cfb1eb4e06f164529f9051c81
rcroft1 <rcroft1@byu.edu>
2023-04-11 13:39:46 -0600

    working cookie

--- Startup.cs
+++ Startup.cs
@@ -81,7 +81,7 @@ namespace EgyptBYU
             app.UseAuthorization();
 
             app.Use(async (context, next) => {
-                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'sha256-m1igTNlg9PL5o60ru2HIIK6OPQet2z9UgiEAhCyg/RU='; style-src 'self' https://cdn.jsdelivr.net; font-src 'self'; img-src 'self'; frame-src 'self'");
+                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'sha256-m1igTNlg9PL5o60ru2HIIK6OPQet2z9UgiEAhCyg/RU='; style-src 'self' https://cdn.jsdelivr.net; font-src 'self'; img-src 'self'; frame-src 'self'");
                 await next();
 
             });
