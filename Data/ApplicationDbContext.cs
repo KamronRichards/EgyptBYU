6da821e703907f70d65754c1a23719ea38446125
ty-327 <tyisb7@gmail.com>
2023-04-12 13:05:44 -0600

    changed connection string and tried a possible fix for migration error

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using EgyptBYU.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,5 +18,29 @@ namespace EgyptBYU.Data
 
         public DbSet<MummyEntity> mummy { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Change data type for TwoFactorEnabled to tinyint
+            builder
+                .Entity<IdentityUser>()
+                .Property(u => u.TwoFactorEnabled)
+                .HasColumnType("tinyint");
+        }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+
+        //    // Create a custom value converter for bool to tinyint
+        //    var boolConverter = new BoolToZeroOneConverter<short>();
+
+        //    // Register the value converter with Entity Framework Core
+        //    builder
+        //        .Entity<IdentityUser>()
+        //        .Property(u => u.TwoFactorEnabled)
+        //        .HasConversion(boolConverter);
+        //}
     }
 }
