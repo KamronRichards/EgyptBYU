141fcede4ee10af70cb383727eab20df86115f00
rcroft1 <rcroft1@byu.edu>
2023-04-13 09:20:42 -0600

    CRUD

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,20 @@ namespace EgyptBYU.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _db;
+        private ApplicationDbContext newContext { get; set; }
 
         public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _db = db;
             _logger = logger;
+            newContext = db;
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
         {
             return View();
         }
@@ -31,6 +37,27 @@ namespace EgyptBYU.Controllers
         {
             IEnumerable<MummyEntity> burialmain = _db.burialmain.ToList();
             return View(burialmain);
+        }
+        [HttpGet][Authorize(Roles ="Admin")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Add(MummyEntity me)
+        {
+            if (ModelState.IsValid)
+            {
+                newContext.Add(me);
+                newContext.SaveChanges();
+
+                return RedirectToAction("burials");
+            }
+            else
+            {
+                return View(me);
+            }
         }
         public IActionResult UnAnalysis()
         {
@@ -45,6 +72,42 @@ namespace EgyptBYU.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet][Authorize(Roles="Admin")]
+        public IActionResult Delete(long id)
+        {
+            var record = newContext.burialmain.Single(m => m.id == id);
+            return View(record);
+        }
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Delete(MummyEntity me)
+        {
+            // identifying and deleting the correct record
+
+            newContext.burialmain.Remove(me);
+            newContext.SaveChanges();
+            return RedirectToAction("burials");
+        }
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Edit(long id)
+        {
+            var burial = newContext.burialmain.Single(m => m.id == id);
+
+            return View("Edit", burial);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Edit(MummyEntity editME)
+        {
+            // identifying and editing the correct record
+
+            newContext.Update(editME);
+            newContext.SaveChanges();
+            return RedirectToAction("Burials");
         }
     }
 }
